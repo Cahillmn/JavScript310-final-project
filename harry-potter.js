11b98ea892e7db5baaba66caf72d8f4b84d6c215
Cahillmn <mcahill@seattleu.edu>
2022-03-22 15:12:46 -0700

    finished final project

--- harry-potter.js
+++ harry-potter.js
@@ -3,22 +3,64 @@ const url = 'https://hp-api.herokuapp.com/api/characters';
 const formEl = document.getElementById("findCharacters");
 const selectEl = document.getElementById('specificName');
 
+// retrieve favorite character from local storage
 
-console.log(formEl);
-console.log(selectEl);
+const favoriteCharacter = document.getElementById('favorite');
+const no = document.getElementById('not-favorite');
+const characterName = localStorage.getItem('favorite');
 
+// if local storage favorite exists, use it as the selected character
+
+if (characterName) { 
+   selectEl.value = characterName;
+} else {
+  console.log('Favorite does not exist')
+}
+
+// run the fetch request when submit button is clicked
 
 formEl.addEventListener('submit', function (e) {
   e.preventDefault();
+
+  //clear prior results from page
+
   const container = document.getElementById('container');
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
+
+  //moves the image to the right
+  const movingImg = document.getElementById('animation');
+  movingImg.style.position = 'relative';
+  movingImg.style.left = '0px';
+
+  const moveRight = () => {
+    if (parseInt(movingImg.style.left) < 1150) {
+      movingImg.style.left = parseInt(movingImg.style.left) + 10 + 'px';
+      requestAnimationFrame(moveRight);
+    }
+
+  };
+
+  moveRight();
+
+  //create a favorite in local storage if yes is selected otherwise delete from local storage
+
+  const nameSelected = selectEl.value;
+
+  const setFavorite = () => {
+    if ((document.getElementById('favorite').checked)) {
+      localStorage.setItem('favorite', nameSelected);
+    } else {
+      localStorage.setItem('favorite', '')
+    }
+  };
+
+  setFavorite();
   
+  // fetching data from API
   const inputVal = selectEl.value;
-  console.log(inputVal);
-  console.log(url);
-  //   // Fetch data from API
+
   fetch(url)
     .then(function (data) {
       return data.json();
@@ -31,8 +73,8 @@ formEl.addEventListener('submit', function (e) {
       const result = characters.filter(function (character) {
         return character.name === inputVal;
       });
-      // if(characters.name = inputVal) {
-      console.log(result);
+      
+      //define values used from API
 
       const finalResult = result[0];
 
@@ -57,7 +99,7 @@ formEl.addEventListener('submit', function (e) {
       const div = document.createElement('div');
       container.appendChild(div);
       div.innerHTML =
-        `<img src= ${image} />
+        `<img class="center" src= ${image} />
                 <h2>${ name }</h2>
                 <div class="characteristics">
                     <p><span>Species: </span>${ species }</p>
